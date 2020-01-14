b460a2db307279049fcb07fd3f53e0f7e40a78d2
cwdott <phil@capitalwebdesign.ca>
2020-01-14 06:26:39 -0500

    making progress
    	modified:   models/ingredient.model.js
    	modified:   routes/routes-pages.js
    	new file:   views/pages/create.ejs
    	new file:   views/pages/index.ejs
    	new file:   views/partials/head.ejs

--- models/ingredient.model.js
+++ models/ingredient.model.js
@@ -1,24 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-let IngredientSchema = new Schema({
-  name: { type: String, required: true, max: 100 },
-  calories: { type: Number },
-  servingText: { type: String, max: 20 },
-  servingAmount: { type: Number },
-  servingUnit: { type: String, max: 5 },
-  fat: { type: Number },
-  fatSaturated: { type: Number },
-  fatTrans: { type: Number },
-  cholesterol: { type: Number },
-  sodium: { type: Number },
-  carbs: { type: Number },
-  fiber: { type: Number },
-  sugar: { type: Number },
-  sugarIncluded: { type: Number },
-  sugarAdded: { type: Number },
-  protein: { type: Number }
-});
+let IngredientSchema = new Schema(
+  {
+    name: { type: String, required: true, max: 100 },
+    calories: { type: Number },
+    servingText: { type: String, max: 20 },
+    servingAmount: { type: Number },
+    servingUnit: { type: String, max: 5 },
+    fat: { type: Number },
+    fatSaturated: { type: Number },
+    fatTrans: { type: Number },
+    cholesterol: { type: Number },
+    sodium: { type: Number },
+    carbs: { type: Number },
+    fiber: { type: Number },
+    sugar: { type: Number },
+    sugarIncluded: { type: Number },
+    sugarAdded: { type: Number },
+    protein: { type: Number }
+  },
+  {
+    timestamps: true
+  }
+);
 
 // Export the model
 module.exports = mongoose.model("Ingredient", IngredientSchema);
--- routes/routes-pages.js
+++ routes/routes-pages.js
@@ -5,8 +5,8 @@ router.get("/", function(req, res) {
   res.render("pages/index");
 });
 
-router.get("/about", function(req, res) {
-  res.render("pages/about");
+router.get("/create", function(req, res) {
+  res.render("pages/create");
 });
 
 module.exports = router;
