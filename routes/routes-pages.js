fee324a66c5450dc29e25a94d7957f47d9432a2e
cwdott <phil@capitalwebdesign.ca>
2020-01-19 23:03:13 -0500

    got list and add working, but refactoring API call broke list

--- routes/routes-pages.js
+++ routes/routes-pages.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const fetch = require("node-fetch");
+
+// const getIngredients = async () => {
+//   try {
+//     const response = await fetch("http://localhost:3000/api/ingredients");
+//     const json = await response.json();
+//     return json;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 router.get("/", function (req, res) {
+  // const ingredients = getIngredients();
+  // console.log("here they are: ", ingredients)
   res.render("pages/index", {
-    pageTitle: "Homepage"
+    pageTitle: "Homepage",
+    // ingredients: ingredients
   });
 });
 
